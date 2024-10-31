0fb8c6f5a69dd8bfeb24fb548cdd1f43e31d2bb4
Aliaksei Kalupaila <alexey2928@gmail.com>
2024-10-30 22:41:23 -0400

    Updated whitespaces.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -12,16 +12,16 @@ const Home = () => {
     <div className="h-screen flex flex-col items-center justify-center space-y-4">
       <h1 className="text-2xl font-semibold">Garden and bee is coming soon!</h1>
 
-      <div className="flex space-x-4 items-center justify-center">
+      <div className="flex space-x-4 items-center justify-center text-base md:text-lg lg:text-xl">
         <a
           href={LINK_BOOK_NOW}
-          className="bg-[#704415] text-white px-6 py-3 rounded-md text-lg font-semibold hover:bg-[#b68b40] transition"
+          className="bg-[#704415] text-white px-6 py-3 rounded-md font-semibold hover:bg-[#b68b40] transition whitespace-nowrap"
         >
           Book Now
         </a>
         <button
           onClick={handleGoBack}
-          className="bg-transparent text-[#704415] px-6 py-3 rounded-md text-lg font-semibold border border-[#704415] hover:bg-[#b68b40] hover:text-white transition"
+          className="bg-transparent text-[#704415] px-6 py-3 rounded-md font-semibold border border-[#704415] hover:bg-[#b68b40] hover:text-white transition whitespace-nowrap"
         >
           Go Back
         </button>
