bd134bd73517b11b2f04b26d6895fef78632cf67
Aliaksei Kalupaila <alexey2928@gmail.com>
2024-12-10 22:50:46 -0500

    Updated link

--- src/helpers/links.js
+++ src/helpers/links.js
@@ -1,5 +1,5 @@
 export const LINK_BOOK_NOW =
-  "https://www.fresha.com/a/lash-and-visage-englishtown-49-county-road-520-dneont3x/booking?allOffer=true";
+  "https://www.fresha.com/book-now/garden-and-bee-g0wbu1pt/services?lid=1484670&eid=3351405&share&pId=1409932";
 export const LINK_INSTAGRAM = "https://www.instagram.com/gardenandbeebeauty";
 export const LINK_TIKTOK = "https://www.tiktok.com/@gardenandbeebeauty";
 export const LINK_GOOGLE_MAPS = "https://maps.app.goo.gl/KqH8RnUMmpPtYWB97";
