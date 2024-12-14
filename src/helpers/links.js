9a65068c55cc569c03171962516ea20eefcd699a
Aliaksei Kalupaila <alexey2928@gmail.com>
2024-12-14 00:18:57 -0500

    Updated google link

--- src/helpers/links.js
+++ src/helpers/links.js
@@ -2,4 +2,4 @@ export const LINK_BOOK_NOW =
   "https://www.fresha.com/book-now/garden-and-bee-g0wbu1pt/services?lid=1484670&eid=3351405&share&pId=1409932";
 export const LINK_INSTAGRAM = "https://www.instagram.com/gardenandbeebeauty";
 export const LINK_TIKTOK = "https://www.tiktok.com/@gardenandbeebeauty";
-export const LINK_GOOGLE_MAPS = "https://maps.app.goo.gl/KqH8RnUMmpPtYWB97";
+export const LINK_GOOGLE_MAPS = "https://maps.app.goo.gl/FY15inJSeKevoHnBA";
